343929cdc3e1e60ade89d6e68676f120255c72af
Juan-Mozo <jmozoignacio@gmail.com>
2021-09-01 11:51:57 -0300

    Week 3 Finished

--- app/src/main/java/com/juanimozo/pronostico/MainActivity.kt
+++ app/src/main/java/com/juanimozo/pronostico/MainActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,10 +30,20 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        // RecyclerView semanal
+        val listaPronostico: RecyclerView = findViewById(R.id.listaPronostico)
+        listaPronostico.layoutManager = LinearLayoutManager(this)
+            // Adaptador al RecyclerView
+        val pronosticoDiarioAdaptador = PronosticoDiarioAdaptador() {  itemPronostico ->
+            val msg = getString(R.string.itemPronostico_seleccionado_formato, itemPronostico.temperatura, itemPronostico.descripcion)
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+        listaPronostico.adapter = pronosticoDiarioAdaptador
+
         // Observador
         val pronosticoSemanalObserver = Observer<List<PronosticoDiario>> { itemsPronostico ->
             // actualiza el adaptador de la lista
-            Toast.makeText(this, "Items Cargador", Toast.LENGTH_SHORT).show()
+            pronosticoDiarioAdaptador.submitList(itemsPronostico)
         }
 
         pronosticoRepositorio.pronosticoSemanal.observe(this, pronosticoSemanalObserver)
---
+++ app/src/main/java/com/juanimozo/pronostico/PronosticoDiarioAdaptador.kt
@@ -0,0 +1,58 @@
+package com.juanimozo.pronostico
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+// ViewHolder
+class PronosticoDiarioViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    // view antes de findViewById porque el adaptador no tiene el contexto al no ser una activity
+    private val temperaturaText = view.findViewById<TextView>(R.id.temperaturaText)
+    private val descripcionText = view.findViewById<TextView>(R.id.descripcionText)
+
+    // Conecta individualmente cada item con las view de arriba
+    fun bind(pronosticoDiario: PronosticoDiario) {
+        temperaturaText.text = String.format("%.1f°", pronosticoDiario.temperatura)
+        descripcionText.text = pronosticoDiario.descripcion
+    }
+}
+
+class PronosticoDiarioAdaptador(
+        private val clickHandler: (PronosticoDiario) -> Unit
+) : ListAdapter<PronosticoDiario, PronosticoDiarioViewHolder>(DIFF_CONFIG) {
+
+    // Movida rara para completar el adaptador ??
+    companion object {
+        val DIFF_CONFIG = object : DiffUtil.ItemCallback<PronosticoDiario>() {
+            //Devuelve solo si oldItem y newItem son lo mismo
+            override fun areItemsTheSame(oldItem: PronosticoDiario, newItem: PronosticoDiario): Boolean {
+                return oldItem === newItem
+            }
+
+            //Devuelve solo si el contenido entre oldItem y newItem son lo mismo
+            override fun areContentsTheSame(oldItem: PronosticoDiario, newItem: PronosticoDiario): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+    // Crea el nuevo ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PronosticoDiarioViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_pronostico_diario, parent, false)
+        return PronosticoDiarioViewHolder(itemView)
+    }
+
+    // Agarra individualmente cada item y lo pasa como grupo al ViewHolder
+    override fun onBindViewHolder(holder: PronosticoDiarioViewHolder, position: Int) {
+        // Accede a los items y los posiciona
+        holder.bind(getItem(position))
+        holder.itemView.setOnClickListener {
+            clickHandler(getItem(position))
+        }
+    }
+}
